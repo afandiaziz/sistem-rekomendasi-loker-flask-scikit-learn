9b4f423cc6a94a05fbcba293d426cc635854effc
Rifaldy Elninoru <rifaldyelninoru@gmail.com>
2023-06-03 22:43:16 +0700

    deleting unused code

--- model/ai.py
+++ model/ai.py
@@ -37,31 +37,5 @@ class ai:
         return document_indices
         
 
-        return "hello"
-
         
-    def recomendation(data,keyword):
-        df = pd.DataFrame(data,columns=["id","title","desc"])
-        df = df.dropna()
-        factory = StopWordRemoverFactory()
-        stopword = factory.create_stop_word_remover()
-        for i, kalimat in enumerate (df.iterrows()):
-            html_pattern = re.compile('<.*?>')
-            df.at[i,"desc"] = stopword.remove(html_pattern.sub(r'',df.loc[i,'desc'].lower()))
-            df.at[i,"title"] = stopword.remove(html_pattern.sub(r'',df.loc[i,'title'].lower()))
-        vectorizer = TfidfVectorizer()
-        tfidf_matrix = vectorizer.fit_transform(df['desc'])
-        feature_names = vectorizer.get_feature_names_out()
-        df_tfidf = pd.DataFrame(tfidf_matrix.toarray(), columns=feature_names)
-        
-
-        new_document = keyword
-        new_tfidf_vector = vectorizer.transform([new_document])
-
-        similarity_scores = cosine_similarity(new_tfidf_vector, tfidf_matrix)
-
-        document_indices = similarity_scores[0].argsort()[::-1]
-        return document_indices
-        
-
-        return "hello"
+    
