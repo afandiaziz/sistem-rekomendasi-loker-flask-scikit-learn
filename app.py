d02a267efe61dea58d6d5608bbdd0586316b0ddf
afandiaziz <afandiaziz46@gmail.com>
2023-08-22 10:47:48 +0700

    feat: system recommendation by keyword

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, request
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-from model.ai import ai
-from flask import jsonify
 import math
-
+import pandas as pd
+from model.ai import ai
+from flask_mysqldb import MySQL
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
@@ -14,47 +12,102 @@ app.config['MYSQL_DB'] = 'bkk'
 
 mysql = MySQL(app)
 
-@app.route('/search/<keyword>')
-def foo(keyword):
-    cur = mysql.connection.cursor()
-    # tambahin parameter
-    query = request.args.get("customquery", "select * from vacancies", type=str)
-    minWeight = request.args.get("min", 0, type=float)
-    cur.execute(query)
-    job = cur.fetchall()
-    allJob = []
-    for data in job:
-        temp = []
-        temp.append( data[0])
-        temp.append( data[2])
-        temp.append( data[3])
-        allJob.append(temp)
-    jobWeight = ai.search(allJob,keyword)
-    sortedJob = jobWeight[0].argsort()[::-1]
-    # eliminate minimum weight
-    x =0
-    for val in sortedJob:
-        print(jobWeight[0][x])
-        if(jobWeight[0][val] < minWeight):
-            print("ini x")
-            print(jobWeight[0][val])
-            print(x)
-            sortedJob = sortedJob[0:x]
-            break
-        x = x+1
-    print(sortedJob)
-    itemPerPage = 5
-    pages = math.ceil(len(sortedJob)/itemPerPage)
-    page = request.args.get("page", 0, type=int)
-    data = []
-    for x in sortedJob[0 if page == 0 else (page-1)*itemPerPage:len(sortedJob) if page == 0 else len(sortedJob) if page >= pages else page*itemPerPage]:
-        temp = {}
-        temp["id"] = allJob[x][0]
-        temp["score"] = jobWeight[0][x]
-        data.append(temp)
 
-    res = {"pages":pages,"data":data}
-    return jsonify(res)
-@app.route('/recomendation')
-def bar():
-    return 'bar!'
+@app.route('/search/<keyword>')
+def recommendationBySearch(keyword):
+    # obtain data from database
+    query = request.args.get("customquery")
+    # tambahin parameter
+    # query = request.args.get(
+    #     "customquery", "select preprocessed_text from vacancies where preprocessed_text_id is not null", type=str)
+
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    vacancies = []
+    for vacancy in cur.fetchall():
+        # vacancy[0] is id
+        # vacancy[2] is position
+        # vacancy[4] is description
+        vacancies.append([vacancy[0], vacancy[5]])
+
+    dframeKeyword = pd.DataFrame([keyword], columns=["text"])
+    dframeVacancies = pd.DataFrame(vacancies, columns=["id", "text"])
+    dframeVacancies = dframeVacancies.dropna()
+
+    vacanciesWeighted = ai.search(dframeVacancies, dframeKeyword)
+    sortedIndexVacancies = vacanciesWeighted.argsort()[::-1]
+
+    # eliminate minimum weight
+    minWeight = request.args.get("min", 0, type=float)
+    for index, vacancyScore in enumerate(vacanciesWeighted[sortedIndexVacancies]):
+        if(vacancyScore < minWeight):
+            # delete element from index 0 to index (but not including this index less than minWeight)
+            sortedIndexVacancies = sortedIndexVacancies[0:index]
+            break
+
+    data = []
+    itemPerPage = 5
+    page = request.args.get("page", 0, type=int)
+    pages = math.ceil(len(sortedIndexVacancies) / itemPerPage)
+    if page == 1 or page == 0:
+        start_index = 0
+        end_index = itemPerPage
+    else:
+        start_index = (page - 1) * itemPerPage
+        if page >= pages:
+            end_index = len(sortedIndexVacancies)
+        else:
+            end_index = page * itemPerPage
+
+    for index in sortedIndexVacancies[start_index:end_index]:
+        data.append({
+            "id": vacancies[index][0],
+            "score": vacanciesWeighted[index]
+        })
+
+    return jsonify({
+        "pages": pages,
+        "data": data
+    })
+
+
+@app.route('/preprocessing', methods=['POST', 'GET'])
+def preprocessingRun():
+    # query = "select id, position, description, preprocessed_text_id from vacancies where preprocessed_text_id is null and deleted_at is null"
+    vacancyId = request.get_json()['vacancy']
+    query = "SELECT id, position, description FROM vacancies WHERE deleted_at is null AND id = '" + \
+        str(vacancyId) + "'"
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    vacancies = []
+    for vacancy in cur.fetchall():
+        # vacancy[0] is id
+        # vacancy[1] is position
+        # vacancy[2] is description
+        text = vacancy[1] + ' ' + vacancy[2]
+        vacancies.append([vacancy[0], text])
+
+    df = pd.DataFrame(vacancies, columns=["id", "text"])
+    df = df.dropna()
+
+    preprocessed = ai.preprocessing(df)
+
+    for i, kalimat in enumerate(preprocessed.iterrows()):
+        updateQuery = "UPDATE vacancies SET preprocessed_text_id = '" + \
+            str(preprocessed.loc[i, 'text']) + "' WHERE id = '" + \
+            str(preprocessed.loc[i, 'id']) + "'"
+        cur.execute(updateQuery)
+        mysql.connection.commit()
+
+    # preprocessedEng = ai.preprocessingEng(preprocessed)
+
+    # for i, kalimat in enumerate(preprocessed.iterrows()):
+    #     updateQuery = "UPDATE vacancies SET preprocessed_text_id_eng = '" + \
+    #         str(preprocessedEng.loc[i, 'text']) + "' WHERE id = '" + \
+    #         str(preprocessedEng.loc[i, 'id']) + "'"
+    #     cur.execute(updateQuery)
+    #     mysql.connection.commit()
+
+    return jsonify({
+        "status": True
+    })
--- model/ai.py
+++ model/ai.py
@@ -1,35 +1,247 @@
-import pandas as pd
+# from nltk.corpus import stopwords
+# from sklearn.metrics.pairwise import linear_kernel
+# from sklearn.feature_extraction.text import CountVectorizer
+# from nltk.tokenize import RegexpTokenizer
+import re
 import nltk
+from html import unescape
 from nltk.corpus import stopwords
-from sklearn.metrics.pairwise import linear_kernel
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from nltk.tokenize import RegexpTokenizer
-import re
-import string
-import random
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 
+
 class ai:
+    nltk.download('punkt')
+    nltk.download('stopwords')
 
-    def search(data,keyword):
-        df = pd.DataFrame(data,columns=["id","title","desc"])
-        df = df.dropna()
-        factory = StopWordRemoverFactory()
-        stopword = factory.create_stop_word_remover()
-        for i, kalimat in enumerate (df.iterrows()):
+    def preprocessing(data):
+        factory = StemmerFactory()
+        stemmer = factory.create_stemmer()
+        stopword = StopWordRemoverFactory().create_stop_word_remover()
+
+        def cleansing(row):
+            text = re.sub(r'&[^\s;&]+;', '', unescape(row['text']))
             html_pattern = re.compile('<.*?>')
-            df.at[i,"desc"] = stopword.remove(html_pattern.sub(r'',df.loc[i,'desc'].lower()))
-            df.at[i,"title"] = stopword.remove(html_pattern.sub(r'',df.loc[i,'title'].lower()))
+            text = html_pattern.sub(r' ', text)
+            text = re.sub(
+                r'((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))', '', text)
+            return text
+
+        def caseFolding(row):
+            text = row['text'].lower()
+            return text
+
+        def tokenizing(row):
+            tokenized = word_tokenize(str(row['text']))
+            return tokenized
+
+        def stemming(row):
+            stemmed = [stemmer.stem(token) for token in row['text']]
+            stemmed = " ".join(stemmed)
+            return stemmed
+
+        def stopwording(row):
+            stopworded = stopword.remove(row['text'])
+            return stopworded
+
+        data['text'] = data.apply(cleansing, axis=1)
+        data['text'] = data.apply(caseFolding, axis=1)
+        data['text'] = data.apply(tokenizing, axis=1)
+        data['text'] = data.apply(stemming, axis=1)
+        data['text'] = data.apply(stopwording, axis=1)
+
+        return data
+
+    def preprocessingEng(data):
+        stemmer = PorterStemmer()
+
+        def cleansing(row):
+            text = re.sub(r'&[^\s;&]+;', '', unescape(row['text']))
+            html_pattern = re.compile('<.*?>')
+            text = html_pattern.sub(r' ', text)
+            text = re.sub(
+                r'((www\.[^\s]+)|(https?://[^\s]+)|(http?://[^\s]+))', '', text)
+            text = text.lower()
+            return text
+
+        def caseFolding(row):
+            text = row['text'].lower()
+            return text
+
+        def tokenizing(row):
+            tokenized = word_tokenize(str(row['text']))
+            return tokenized
+
+        def stemming(row):
+            stemmed = [stemmer.stem(token) for token in row['text']]
+            stemmed = " ".join(stemmed)
+            return stemmed
+
+        def stopwording(row):
+            stopword = set(stopwords.words('english'))
+            tokens = word_tokenize(str(row['text']))
+            stopworded = [token for token in tokens if token not in stopword]
+            stopworded = " ".join(stopworded)
+            return stopworded
+
+        data['text'] = data.apply(cleansing, axis=1)
+        data['text'] = data.apply(caseFolding, axis=1)
+        data['text'] = data.apply(tokenizing, axis=1)
+        data['text'] = data.apply(stemming, axis=1)
+        data['text'] = data.apply(stopwording, axis=1)
+
+        return data
+
+    def search(data, keyword):
+        # cursor = mysql.connection.cursor()
+        # cursor.execute("TRUNCATE `cbrs_words`")
+        # mysql.connection.commit()
+
+        # cursor.execute("TRUNCATE `cbrs_tfidf`")
+        # mysql.connection.commit()
+
+        # cursor.execute("TRUNCATE `cbrs_result`")
+        # mysql.connection.commit()
+        keyword = ai.preprocessing(keyword)
+
         vectorizer = TfidfVectorizer()
-        tfidf_matrix = vectorizer.fit_transform(df['desc'])
-        
-        new_tfidf_vector = vectorizer.transform([keyword])
+        tfidf_matrix = vectorizer.fit_transform(data['text'])
+        new_tfidf_vector = vectorizer.transform(keyword["text"])
 
+        print()
+        print()
+        # print(new_tfidf_vector)
+        # for word_index, word in enumerate(words):
+        #     query = "INSERT INTO `cbrs_words` (`word`) VALUES('" + \
+        #         str(word) + "')"
+        #     query2 = "INSERT INTO `cbrs_tfidf` (`word_id`, `tf`, `idf`) VALUES('" + str(word_index) + "', '" + str(
+        #         document_frequencies[word_index]) + "', '" + str(idf_values[word_index]) + "')"
+        #     cursor.execute(query)
+        #     mysql.connection.commit()
+        #     cursor.execute(query2)
+        #     mysql.connection.commit()
+
+        # for indexColumn, columnName in enumerate(dfCombine):
+        #     for word_index, word in enumerate(words):
+        #         tf = tfidf_matrix_count[indexColumn, word_index]
+        #         tfidf = float(
+        #             tfidf_matrix_count[indexColumn, word_index]) * float(idf_values[word_index])
+        #         query = "INSERT INTO `cbrs_result` (`word_id`, `document_id`, `tf`, `tfidf`, `normalized_tfidf`) VALUES('" + str(
+        #             word_index) + "', '" + str(indexColumn) + "', '" + str(tf) + "', '" + str(tfidf) + "', '" + str(tfidf_matrix.toarray()[indexColumn, word_index]) + "')"
+        #         cursor.execute(query)
+        #         mysql.connection.commit()
+        # if indexColumn == 0:
+        #     continue
+        # elif indexColumn > len(dfCombine):
+        #     if indexColumn == len(dfCombine):
+        #         indexColumnTemp = 0
+
+        #     for word_index, word in enumerate(words):
+        #         printDataFrame.at[word_index,
+        #                           columnName] = new_tfidf_vector_count[indexColumnTemp, word_index]
+        # else:
+        #     for word_index, word in enumerate(words):
+        #         printDataFrame.at[word_index,
+        #                           columnName] = tfidf_matrix_count[indexColumnTemp, word_index]
+
+        # indexColumnTemp += indexColumnTemp
+        print()
+        print()
         similarity_scores = cosine_similarity(new_tfidf_vector, tfidf_matrix)
-        return similarity_scores
-        
+        return similarity_scores[0]
 
-        
-    
+    # def search(data, keyword):
+
+    #     print("")
+    #     print("")
+
+    #     data = [
+    #         "Web Developer <p>Kami sedang mencari seorang Web Developer yang berpengalaman untuk bergabung dengan tim kami. Tanggung jawab Anda akan mencakup pengembangan front-end dan back-end, serta memastikan keberlanjutan dan performa tinggi dari situs web kami.</p> <h2>Kualifikasi:</h2> <ul> <li>Pengalaman dalam HTML, CSS, dan JavaScript</li> <li>Pemahaman yang baik tentang desain responsif</li> <li>Pengalaman dalam pengembangan situs web menggunakan CMS seperti WordPress</li> <li>Kemampuan untuk bekerja dalam tim</li> </ul>",
+    #         "Data Scientist <p>Kami mencari seorang Data Scientist yang handal dan berpengalaman untuk bergabung dengan tim kami. Tugas Anda akan meliputi analisis data, pengembangan model prediktif, dan penemuan wawasan berharga dari set data yang kompleks.</p> <h2>Kualifikasi:</h2> <ul> <li>Pengalaman dalam analisis data menggunakan Python atau R</li> <li>Pemahaman yang kuat tentang statistik dan metode analisis data</li> <li>Pengalaman dalam penggunaan alat-alat seperti SQL dan Pandas</li> <li>Kemampuan komunikasi yang baik untuk menyampaikan hasil analisis dengan jelas</li> </ul>",
+    #         "UI/UX Designer <p>Kami sedang mencari seorang UI/UX Designer yang kreatif dan berbakat untuk bergabung dengan tim kami. Tanggung jawab Anda akan meliputi merancang antarmuka pengguna yang menarik dan fungsional, serta melakukan pengujian pengguna dan iterasi desain.</p> <h2>Kualifikasi:</h2> <ul> <li>Pengalaman dalam merancang antarmuka pengguna yang menarik</li> <li>Pemahaman tentang prinsip-prinsip desain UI/UX</li> <li>Kemampuan menggunakan alat desain seperti Adobe XD atau Sketch</li> <li>Kemampuan untuk bekerja dalam tim dan berkolaborasi dengan pengembang</li> </ul>",
+    #         "Front-end Developer <p>Kami mencari seorang Front-end Developer yang berpengalaman untuk bergabung dengan tim pengembangan kami. Anda akan bertanggung jawab dalam mengembangkan antarmuka pengguna interaktif menggunakan HTML, CSS, dan JavaScript serta memastikan kinerja optimal dan responsif dari situs web kami.</p> <h2>Kualifikasi:</h2> <ul> <li>Pengalaman dalam pengembangan antarmuka pengguna menggunakan HTML, CSS, dan JavaScript</li> <li>Pemahaman yang baik tentang desain responsif dan pengujian lintas browser</li> <li>Pengalaman dengan kerangka kerja (framework) seperti React atau Vue.js</li> <li>Kemampuan untuk bekerja dalam tim dan berkolaborasi dengan desainer UI/UX</li> </ul>",
+    #         "Front-end Web Developer <ul> <li><strong>Deskripsi:</strong></li> <ul> <li>Mengembangkan antarmuka pengguna (UI) yang menarik dan responsif untuk website.</li> <li>Mengimplementasikan desain web menggunakan HTML, CSS, dan JavaScript.</li> <li>Memastikan kesesuaian lintas browser (cross-browser compatibility) pada antarmuka pengguna.</li> <li>Optimasi kinerja website agar responsif dan cepat diakses oleh pengguna.</li> <li>Kolaborasi dengan tim pengembang dan desainer untuk menghasilkan pengalaman pengguna terbaik.</li> <li>Menggunakan framework dan alat pengembangan front-end seperti React, Angular, atau Vue.js.</li> </ul> <li><strong>Kualifikasi:</strong></li> <ul> <li>Pemahaman yang kuat tentang HTML, CSS, dan JavaScript.</li> <li>Pengalaman dengan framework front-end seperti React, Angular, atau Vue.js merupakan nilai tambah.</li> <li>Kemampuan dalam desain responsif dan lintas browser compatibility.</li> <li>Kemampuan pemecahan masalah dan pemikiran kreatif dalam menghadapi tantangan pengembangan front-end.</li> <li>Kemampuan kerja dalam tim yang baik dan komunikasi yang efektif.</li> </ul> </ul>",
+    #         "Back-end Web Developer <ul> <li><strong>Deskripsi:</strong></li> <ul> <li>Memiliki tanggung jawab dalam pengembangan logika aplikasi web sisi server.</li> <li>Integrasi antarmuka pengguna (UI) dengan elemen-elemen sisi depan.</li> <li>Mengembangkan API (Application Programming Interface) untuk komunikasi dengan aplikasi lain.</li> <li>Mengelola basis data dan memastikan keamanan serta efisiensi dalam pengaksesan data.</li> <li>Menerapkan praktik keamanan dalam pengembangan aplikasi web.</li> <li>Pemecahan masalah dan debugging pada sisi server.</li> </ul> <li><strong>Kualifikasi:</strong></li> <ul> <li>Pemahaman yang kuat tentang bahasa pemrograman sisi server seperti Java, Python, atau PHP.</li> <li>Pengalaman dengan pengembangan API (Application Programming Interface).</li> <li>Pengalaman dengan pengelolaan basis data dan SQL.</li> <li>Pemahaman tentang praktik keamanan dalam pengembangan aplikasi web.</li> <li>Kemampuan pemecahan masalah dan pemikiran analitis.</li> <li>Kemampuan kerja dalam tim dan komunikasi yang baik.</li> </ul> </ul>",
+    #     ]
+
+    #     # data = ['Ini adalah contoh dokumen pertama.', 'Ini adalah contoh dokumen kedua.', 'Ini adalah contoh dokumen ketiga.',
+    #     #         'Ini adalah contoh dokumen keempat.', 'Saya suka makan nasi goreng', 'Nasi goreng adalah makanan favorit saya', 'Saya makan nasi goreng setiap hari', ]
+
+    #     # df = pd.DataFrame(data, columns=["texttt"])
+    #     # df = df.dropna()
+    #     # # menghapus stop words (kata-kata umum yang sering muncul dan biasanya tidak memiliki makna penting dalam analisis teks)
+    #     stopword = StopWordRemoverFactory().create_stop_word_remover()
+    #     for i, text in enumerate(data):
+    #         html_pattern = re.compile('<.*?>')
+    #         data[i] = stopword.remove(html_pattern.sub(r'', text.lower()))
+
+    #         # df.at[i, "texttt"] = stopword.remove(
+    #         #     html_pattern.sub(r'', df.loc[i, 'texttt'].lower())
+    #         # )
+
+    #     # vectorizer = TfidfVectorizer()
+
+    #     # vectorizer.fit(df['texttt'])
+    #     # tfidf_matrix = vectorizer.fit_transform(df['texttt'])
+    #     # new_tfidf_vector = vectorizer.transform(["contoh nasi"])
+    #     # similarity_scores = cosine_similarity(new_tfidf_vector, tfidf_matrix)
+    #     # # Mengurutkan indeks berdasarkan similarity score terbesar
+    #     # sorted_indices = similarity_scores.argsort()[0][::-1]
+    #     # # Dokumen dengan similarity score terbesar
+    #     # most_similar_doc_index = sorted_indices[0]
+    #     # most_similar_score = similarity_scores[0][most_similar_doc_index]
+    #     # most_similar_doc = data[most_similar_doc_index]
+
+    #     # # print(f"Dokumen baru {doc_index+1}: {tfidf_score}")
+    #     # print(f"Dokumen yang paling mirip: {most_similar_doc}")
+    #     # print(f"Skor similarity: {most_similar_score}")
+    #     # print(similarity_scores)
+    #     # print(sorted_indices)
+    #     # vectorizer.fit(data)
+    #     # tfidf_matrix = vectorizer.fit_transform(data)
+    #     # new_tfidf_vector = vectorizer.transform(["nasi"])
+    #     # Melakukan fitting dan transformasi pada data dokumen
+    #     # tfidf_matrix = vectorizer.fit_transform(data)
+
+    #     # Mendapatkan daftar fitur kata unik
+    #     # feature_names = vectorizer.get_feature_names_out()
+
+    #     # # Menampilkan hasil
+    #     # for doc_index, doc in enumerate(data):
+    #     #     print(f"Dokumen {doc_index+1}:")
+    #     #     for feature_index in tfidf_matrix[doc_index].indices:
+    #     #         print(
+    #     #             f"{feature_names[feature_index]}: {tfidf_matrix[doc_index, feature_index]}")
+    #     #     print()
+
+    #     # # vectorizer.get_feature_names_out()
+    #     # # print(tfidf_matrix)
+    #     # print("")
+    #     # print("")
+
+    #     # keyword = "contoh"
+    #     # new_tfidf_matrix = vectorizer.transform([keyword])
+    #     # # keyword_index = feature_names.index(keyword)
+
+    #     # # Menampilkan hasil
+    #     # print(f"Kata kunci: {keyword}")
+    #     # # Menampilkan hasil transformasi dokumen baru
+    #     # for doc_index, doc in enumerate(new_documents):
+    #     #     print(f"Dokumen baru {doc_index+1}:")
+    #     #     for feature_index in new_tfidf_matrix[doc_index].indices:
+    #     #         print(
+    #     #             f"{feature_names[feature_index]}: {new_tfidf_matrix[doc_index, feature_index]}")
+    #     #     print()
+
+    #     print("")
+    #     print("")
+    #     return data
+    # # new_tfidf_vector = vectorizer.transform([keyword])
+    # similarity_scores = cosine_similarity(new_tfidf_vector, tfidf_matrix)
+
+    # # return similarity_scores[0]
